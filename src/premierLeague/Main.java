c005c2dd506c087dbc5f8dd4e36f15b225bacbf1
kibragimov <rusmirolub@gmail.com>
2020-11-30 17:31:31 +0000

    Creating JButtons in java

--- src/premierLeague/Main.java
+++ src/premierLeague/Main.java
@@ -1,6 +1,5 @@
 package premierLeague;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JPanel;
 
@@ -8,28 +7,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		String myLabelString = "My name is Kamol";
-		MyLabel myLabel = new MyLabel(myLabelString, 200, 100);
+		// JPanel = a GUI components that is a container for other components
+//		
+//		JPanel redPanel = new JPanel();
+//		redPanel.setBackground(Color.red);
+//		redPanel.setBounds(300, 0, 200, 200);
+//		redPanel.setLayout(null);
+//		
+//		JPanel bluePanel = new JPanel();
+//		bluePanel.setBackground(Color.blue);
+//		bluePanel.setBounds(300, 250, 200, 200);
+//		
+//		JPanel greenPanel = new JPanel();
+//		greenPanel.setBackground(Color.green);
+//		greenPanel.setBounds(300,500, 200, 200);
 		
-		JPanel redPanel = new JPanel();
-		redPanel.setBackground(Color.red);
-		redPanel.setBounds(300, 0, 200, 200);
-		redPanel.setLayout(null);
+		MyFrame myFrame = new MyFrame("Premier League", 400, 400);
 		
-		JPanel bluePanel = new JPanel();
-		bluePanel.setBackground(Color.blue);
-		bluePanel.setBounds(300, 250, 200, 200);
-		
-		JPanel greenPanel = new JPanel();
-		greenPanel.setBackground(Color.green);
-		greenPanel.setBounds(300,500, 200, 200);
-		
-		MyFrame myFrame = new MyFrame(800, 800);
-		myFrame.setLayout(null);
-		redPanel.add(myLabel);
-		myFrame.add(redPanel);
-		myFrame.add(greenPanel);
-		myFrame.add(bluePanel);
+//		redPanel.add(myLabel);
+//		myFrame.add(redPanel);
+//		myFrame.add(greenPanel);
+//		myFrame.add(bluePanel);
 		
 		myFrame.revalidate();
 	}
--- src/premierLeague/MyFrame.java
+++ src/premierLeague/MyFrame.java
@@ -1,23 +1,63 @@
 package premierLeague;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
+	
+	JButton myButton;
+	MyLabel myLabel;
 
-	MyFrame(int width, int height) {
+	MyFrame(String title, int width, int height) {
+		
 		// JFrame = a GUI window to add components to. It contains all components
 
-		this.setBounds(40, 40, width, height);  // This sets width and height of this
-		this.setTitle("Premier League");
+		this.createButton();
+		this.createLabel();
+		
+		this.setBounds(0, 0, width, height);  // This sets width and height of this
+		this.setTitle(title);
+		this.setLayout(null);
 
         // By default it is HIDE_ON_CLOSE,
 		// there are also DO_NOTHING_ON_CLOSE
 		this.setDefaultCloseOperation(MyFrame.EXIT_ON_CLOSE); // exit out of application
-
 		this.setResizable(true); // prevent being resized
 		this.setVisible(true); // Make this visible on screen
 		this.getContentPane().setBackground(new Color(123,50,49)); // set background colour
+	}
+	
+	private void createButton() {
+		this.myButton = new JButton();
+		myButton.setBounds(150,100, 100, 50);
+		/* myButton.addActionListener(e -> System.out.println("Hii")); // lambda */
+		myButton.addActionListener(this);
+		myButton.setText("Click me!");
+//		myButton.setFocusable(true);
+		myButton.setFont(new Font("Comic sans", Font.BOLD, 15));
+		this.add(this.myButton);
+	}
+	
+	private void createLabel() {
+		String myLabelString = "My name is Kamol";
+		this.myLabel = new MyLabel(myLabelString, 200, 100);
+		this.add(this.myLabel);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == this.myButton) {
+			if (this.myLabel.isVisible()) {
+				this.myLabel.setVisible(false);
+			} else {
+				this.myLabel.setVisible(true);
+			}
+			
+		}
 	}
 }
