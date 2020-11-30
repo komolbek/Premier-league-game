f9b5becb6a1cd758b2cec5a876219574f5819d78
kibragimov <rusmirolub@gmail.com>
2020-11-30 16:49:46 +0000

    Creating JLabels in Java

--- src/premierLeague/Main.java
+++ src/premierLeague/Main.java
@@ -1,11 +1,40 @@
 package premierLeague;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.border.Border;
+
 public class Main {
 
 	public static void main(String[] args) {
+	
+		Border border = BorderFactory.createLineBorder(Color.green, 3); // Create border
+		
+		// JLabel = a GUI component to display area for text, image or both
+		
+		JLabel label = new JLabel(); // Create a label
+		label.setText("My name is Kamol and I like building in Java"); // Set text of the label
+		label.setForeground(Color.cyan);
+		
+		// label.setHorizontalTextPosition(JLabel.RIGHT); // Set text left, right, centre of icon
+		// label.setVerticalTextPosition(JLabel.CENTER); // Set text top, bottom, centre of icon
+		label.setFont(new Font("MV Voli", Font.PLAIN, 20)); // Set font of text and size
+		label.setBackground(Color.darkGray); // Set background colour
+		label.setOpaque(true); // Display background colour
+		label.setBorder(border); // Set border 
+		label.setHorizontalAlignment(JLabel.CENTER); // Set alignment
+		label.setVerticalAlignment(JLabel.CENTER); // Set alignment
+		label.setBounds(0, 0, 250, 200); // Set x, y and  width and  height
 
 		// MyFrame myFrame = new MyFrame();
-		new MyFrame();
+		MyFrame myFrame = new MyFrame();
+		myFrame.setLayout(null);
+		myFrame.add(label);
+		myFrame.revalidate();
 	}
 
 }
--- src/premierLeague/MyFrame.java
+++ src/premierLeague/MyFrame.java
@@ -9,14 +9,14 @@ public class MyFrame extends JFrame {
 	MyFrame() {
 		// JFrame = a GUI window to add components to. It contains all components
 
-		this.setSize(420, 420); // This sets width and height of this
+		this.setBounds(40, 40, 500, 500);  // This sets width and height of this
 		this.setTitle("Premier League");
 
         // By default it is HIDE_ON_CLOSE,
 		// there are also DO_NOTHING_ON_CLOSE
-		this.setDefaultCloseOperation(MyFrame.DO_NOTHING_ON_CLOSE); // exit out of application
+		this.setDefaultCloseOperation(MyFrame.EXIT_ON_CLOSE); // exit out of application
 
-		this.setResizable(false); // prevent being resized
+		this.setResizable(true); // prevent being resized
 		this.setVisible(true); // Make this visible on screen
 		this.getContentPane().setBackground(new Color(123,50,49)); // set background colour
 	}
