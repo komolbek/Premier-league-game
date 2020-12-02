994f1748212411562311980069897a2c8d3158e5
kibragimov <rusmirolub@gmail.com>
2020-12-02 17:57:39 +0000

    Implemented PL table sorting by club points

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
-import CustomUI.MyJTable;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
 	public static void main(String[] args) {
 		PremierLeagueManager premierLeagueManager = new PremierLeagueManager();
 		premierLeagueManager.showUserOptions();
 		
+		Collections.sort(premierLeagueManager.getfootballClubs(), new Comparator<FootballClub>() {
+	        public int compare(FootballClub p1, FootballClub p2) {
+	           return Integer.valueOf(p2.getPoints()).compareTo(p1.getPoints());
+	        }
+		});
+		
 		// Custom created JTable object. Custom classes are efficient to be reused 
-		MyJTable myJTable = new MyJTable();
+		MyTableView myTableView = new MyTableView(premierLeagueManager.getfootballClubs());
+		myTableView.revalidate();
 	}
 }
