e0213044b05157c1a0b3a0369f3a9d4f7d42da2f
kibragimov <rusmirolub@gmail.com>
2020-12-03 12:44:07 +0000

    Moved sorting logic to MyTableView class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
-import java.util.Collections;
-import java.util.Comparator;
 
 public class Main {
 	public static void main(String[] args) {
 		PremierLeagueManager premierLeagueManager = new PremierLeagueManager();
 		premierLeagueManager.showUserOptions();
-		
-		Collections.sort(premierLeagueManager.getfootballClubs(), new Comparator<FootballClub>() {
-	        public int compare(FootballClub p1, FootballClub p2) {
-	           return Integer.valueOf(p2.getPoints()).compareTo(p1.getPoints());
-	        }
-		});
-		
+	
 		// Custom created JTable object. Custom classes are efficient to be reused 
 		MyTableView myTableView = new MyTableView(premierLeagueManager.getfootballClubs());
 		myTableView.revalidate();
