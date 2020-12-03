5b455c7f5146124a730d979de80f15201255936f
kibragimov <rusmirolub@gmail.com>
2020-12-03 11:50:49 +0000

    Removed redundant imports. Combined some commons

--- src/MyTableView.java
+++ src/MyTableView.java
@@ -2,22 +2,13 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.nio.channels.NonWritableChannelException;
 import java.util.ArrayList;
 
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane; 
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.border.Border;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 /**
